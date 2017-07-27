f7c766fd60354fc413c68e8ab1871655a1bb9e79
Zadorozhnyi <Zadorozhnyi@reksoft.ru>
2017-07-27 17:20:19 +0300

    Added Quick sort implementation (not working correctly) Added array and QuickSort actions in Main class

--- src/main/java/algorythms/sort/Main.java
+++ src/main/java/algorythms/sort/Main.java
@@ -1,10 +1,12 @@
 package algorythms.sort;
 
 import java.util.ArrayList;
+import java.util.List;
 
 public class Main {
     public static void main(String[] args) {
         ArrayList<Integer> arrayList = new ArrayList<>(10);
+        ArrayList<Integer> arrayListForQuickSort = new ArrayList<>(100);
         arrayList.add(4);
         arrayList.add(2);
         arrayList.add(1);
@@ -16,11 +18,17 @@ public class Main {
 
         for (int i = 0; i < 25; i++) {
             arrayList.add((int) (Math.random() * 1000));
+            arrayListForQuickSort.add((int) (Math.random() * 100));
         }
 
         BubbleSort bubbleSort = new BubbleSort(arrayList);
         bubbleSort.display();
         bubbleSort.sortArray();
         bubbleSort.display();
+
+        QuickSort quickSort = new QuickSort(arrayListForQuickSort);
+        quickSort.display();
+        quickSort.sortArray();
+        quickSort.display();
     }
 }
--- src/main/java/algorythms/sort/QuickSort.java
+++ src/main/java/algorythms/sort/QuickSort.java
@@ -1,4 +1,53 @@
 package algorythms.sort;
 
+import java.util.List;
+
 public class QuickSort {
+    List<Integer> arrayList;
+
+    public QuickSort(List<Integer> arrayList) {
+        this.arrayList = arrayList;
+    }
+
+    public void sortArray() {
+        recursiveQuickSort(0, arrayList.size() - 1);
+    }
+
+    private void recursiveQuickSort(int leftBound, int rightBound) {
+        if (rightBound - leftBound <= 0) {
+            return;
+        } else {
+            int pivot = arrayList.get(rightBound);
+            int partitionIndex = partition(leftBound, rightBound, pivot);
+            recursiveQuickSort(leftBound, partitionIndex - 1);
+            recursiveQuickSort(partitionIndex + 1, rightBound);
+        }
+    }
+
+    private int partition(int leftBound, int rightBound, Integer pivot) {
+        int leftCursor = leftBound - 1;
+        int rightCursor = rightBound + 1;
+        while (true) {
+            while (leftCursor < rightBound && arrayList.get(++leftCursor) < pivot) {}
+
+            while (rightCursor > leftBound && arrayList.get(--rightCursor) > pivot) {}
+
+            if (leftCursor >= rightCursor) {
+                break;
+            } else {
+                swapElements(leftCursor, rightCursor);
+            }
+        }
+        return leftCursor;
+    }
+
+    private void swapElements(int firstIndex, int secondIndex) {
+        int tempValue = arrayList.get(firstIndex);
+        arrayList.set(firstIndex, arrayList.get(secondIndex));
+        arrayList.set(secondIndex, tempValue);
+    }
+
+    public void display() {
+        System.out.println(arrayList.toString());
+    }
 }
